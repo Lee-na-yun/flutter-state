f552123e3540e2ba1ea3ed5f8401166669eecadc
Lee-na-yun <happyin3333@nate.com>
2022-11-15 09:41:18 +0900

    Riverpod provider 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// provider -> 공급자
+// provider는 창고(Repository)에 데이터를 공급
+final numProvider = Provider((_) => 2);
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    // 위젯에서 프로바이더를 사용하고 읽기위해
+    // 앱 전체적으로 "ProviderScope" 위젯을 감싸줘야 합니다.
+    // 여기에 프로바이더의 상태가 저장됩니다.
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -48,12 +60,17 @@ class _HomePageState extends State<HomePage> {
 }
 
 // a컴포넌트 // 컨슈머(데이터 소비자) - 상태를 가지고 그림을 그림
-class AComponent extends StatelessWidget {
+// StatelessWidget 대신에 Riverpod의 ConsumerWidget을 상속받아 사용합니다. (다시 그리고싶은 것에 사용)
+class AComponent extends ConsumerWidget {
   final int num;
   const AComponent(this.num, {Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // WidgetRef ref로 provider에 접근 가능 (= 공급자에게 접근 -> 공급자가 데이터 가져옴)
+    // 소비를 한번만 할 때 read 사용!
+    int num = ref.read(numProvider);
+
     return Container(
       color: Colors.yellow,
       child: Column(
@@ -63,7 +80,7 @@ class AComponent extends StatelessWidget {
             // TEXT를 Expanded하면 text가 커지니까 align으로 정렬 후 확장!
             child: Align(
               child: Text(
-                "${num}",
+                "${num}", // num 바인딩
                 style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
             ),
